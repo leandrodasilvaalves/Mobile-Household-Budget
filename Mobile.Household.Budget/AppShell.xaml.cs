a212a74f73eaa87aebccc78fe6ae91b7d020bfdf
Leandro Alves <leandro.silva.alves86@gmail.com>
2024-02-14 10:55:15 -0300

    Incluir configuracao inicial do projeto

---
+++ Mobile.Household.Budget/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Mobile.Household.Budget;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Mobile.Household.Budget/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Mobile.Household.Budget;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
